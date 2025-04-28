3177bb4237cdbefda9529f73a8b9f8f584794eea
GorioshiGirl <yukkie113@gmail.com>
2025-04-28 16:46:36 +0900

    Update app.py
    Return 200 when received request

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    print(request.json)  # LINEからのデータをターミナルに表示
-    return 'OK'
+@app.route("/", methods=["POST"])
+def webhook():
+    # LINEからのWebhookイベントを受け取ったときの処理
+    print(request.json)  # 受け取ったデータを表示
+    return "OK", 200  # ステータスコード200を返す
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=10000)
